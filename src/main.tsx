3c94dbd0679753e2eacfd33757668469a7524b47
Tesfaye Lemi <tesfu9503@gmail.com>
2023-03-16 17:11:02 +0300

    some configration changed on category page

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
